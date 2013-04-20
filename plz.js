9c37487ee780de18d8ffe3c4bde5713c91b88a96
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-04-20 16:04:19 +0200

    Update plz.js

--- plz.js
+++ plz.js
@@ -183,7 +183,7 @@ function f_toggleStrictMode(data) {
 }
   antispam = function(chat) {
     var plugRoomLinkPatt, sender;
-    plugRoomLinkPatt = /(\bhttps?:\/\/(www.)?adf\.ly[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    plugRoomLinkPatt = /(\bhttps?:\/\/(www.)?adf.ly[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     if (plugRoomLinkPatt.exec(chat.message)) {
       sender = API.getUser(chat.fromID);
       if (!sender.ambassador && !sender.moderator && !sender.owner && !sender.superuser) {
